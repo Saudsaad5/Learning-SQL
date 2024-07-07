c8a5caca3f6b7fe597933648d5b052f98f3dc211
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-07-07 10:40:42 +0300

    Inserting rows in SQL

---
+++ Insert.sql
@@ -0,0 +1,9 @@
+-- to insert a row in a table:
+insert into Employees
+values (1,'Saud','Saad',25.99,'2024-06-24','0551666387'),
+       (2,'abudlla','abed',20.89,'2024-07-27','0559590404'),
+       (3,'azad','mustafa',40.50,'2024-5-26','0503741839');
+
+-- to insert some of the data:
+isert into Employees(employee_id,first_name,last_name)
+values (4, 'ahmed', 'mohsen');
